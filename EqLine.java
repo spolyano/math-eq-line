b4a13cf29ce3b656d1ca6cf8c0bcd6ddd702c9a1
k4despair <k4despair@yandex.ru>
2019-02-02 16:17:30 +0000

    Semantics and cosmetic corrections + Delete decorated input

--- EqLine.java
+++ EqLine.java
@@ -3,10 +3,10 @@ import java.io.InputStream;
 
 public class EqLine {
 	
-	private static final int DEGREE = 2;
+	private static final int DEGREE = 1;
 	
-	private double[] coeff = new double[DEGREE];
-	private double[] root = new double[DEGREE - 1];
+	private double[] coeff = new double[DEGREE + 1];
+	private double[] root = new double[DEGREE];
 
 	private String eqStr = "ax + b = 0";
 	
@@ -14,30 +14,17 @@ public class EqLine {
 	private solutionType solution;
 
 	public EqLine() {};
-	
-	public void init() {
-		Scanner keyRead = new Scanner(System.in);
-		char label;
-		int i;
-		for (i=0, label='a'; i<coeff.length; i++, label++){
-			System.out.print(label + " = ");
-			coeff[i] = (keyRead.hasNextDouble()? keyRead.nextDouble() : 0);
-		}
 
-		keyRead.close();
-	}
-	
 	public void init(InputStream inStr)
 	{
-		Scanner keyRead = new Scanner(System.in);
-		for (int i=0; (i<coeff.length) && keyRead.hasNextDouble(); i++){
-			coeff[i] = keyRead.nextDouble();
-		}
+		Scanner keyRead = new Scanner(inStr);
+		for (int i=0; (i<coeff.length) && keyRead.hasNextDouble(); i++) coeff[i] = keyRead.nextDouble();
 		keyRead.close();
+
+		eqStr = coeff[0] + "x " + ( (Math.signum(coeff[1])<0)? "-":"+" ) + " " + Math.abs(coeff[1]) + " = 0";
 	}
 	
 	public void calculate() {
-		eqStr = coeff[0] + "x " + ( (Math.signum(coeff[1])<0)? "-":"+" ) + " " + Math.abs(coeff[1]) + " = 0";
 		if (coeff[0]!=0) {
 			solution = solutionType.NORMAL;
 			root[0] = (-coeff[1])/coeff[0];
@@ -52,8 +39,7 @@ public class EqLine {
 	
 	public String printRoot() {
 		String res = "";
-		int i;
-		for (i=0; i<root.length; i++) res+= "x = " + root[i] + "\n";
+		for (int i=0; i<root.length; i++) res+= "x = " + root[i] + "\n";
 		return res;
 	}
 	
